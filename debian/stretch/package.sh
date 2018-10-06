a0858f1e618f7300fa64112c34080f19348ad4fc
David Lamparter <equinox@diac24.net>
2018-10-06 23:20:01 +0200

    debian: unify package.sh

--- debian/stretch/package.sh
+++ debian/stretch/package.sh
@@ -6,11 +6,13 @@ ln -s debianpkg debian
 
 if test -f /usr/lib/*/pkgconfig/rtrlib.pc; then
 	sed -e 's%WANT_RPKI ?= 0%WANT_RPKI = 1%' -i debian/rules
-	profile="-P pkg.frr.rtrlib"
+	profile="--build-profiles=pkg.frr.rtrlib"
 fi
 
 sed -e 's%WANT_SNMP ?= 0%WANT_SNMP = 1%' -i debian/rules
 
 dpkg-buildpackage -b $profile
 cd ..
-mv *.deb *.buildinfo *.changes output/
+mv *.deb output/
+test *.buildinfo != '*.buildinfo' && mv *.buildinfo output/
+test *.changes   != '*.changes'   && mv *.changes   output/
