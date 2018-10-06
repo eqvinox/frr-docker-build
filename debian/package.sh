214839f44a01c42d617673f08e85097cb691f07a
David Lamparter <equinox@diac24.net>
2018-10-07 01:09:54 +0200

    debian: combine builds
    Unified Dockerfile works for jessie, stretch and buster.

--- debian/package.sh
+++ debian/package.sh
@@ -4,9 +4,11 @@ tar zxvf frr-dist.tar.gz
 cd frr-dist
 ln -s debianpkg debian
 
-if test -f /usr/lib/*/pkgconfig/rtrlib.pc; then
+if pkg-config --exists rtrlib; then
 	sed -e 's%WANT_RPKI ?= 0%WANT_RPKI = 1%' -i debian/rules
-	profile="--build-profiles=pkg.frr.rtrlib"
+	profile="-Ppkg.frr.rtrlib"
+else
+	profile="-Ppkg.frr.nortrlib"
 fi
 
 sed -e 's%WANT_SNMP ?= 0%WANT_SNMP = 1%' -i debian/rules
